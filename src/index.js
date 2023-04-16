b6fe86d4a5c74ff7a9b01523950dc8a1bcf7063c
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-04-16 18:11:47 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import 'modern-normalize';
+
+import { ThemeProvider } from '@emotion/react';
+
+const theme = {
+  colors: {
+    black: '#000000',
+    text: '#212121',
+    accent: 'orangered',
+  },
+  radii: {
+    xs: '2px',
+    sm: '4px',
+    md: '8px',
+    lg: '16px',
+  },
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
