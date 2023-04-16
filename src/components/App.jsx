570ff8ae53753337cfe7175b58aa804dede332fc
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-04-16 18:11:06 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
-import { RecipeList } from './RecipeList';
+import { RecipeList } from './RecipeList/RecipeList';
 import recipes from '../recipes.json';
+import { Layout } from './Layout/Layout';
 
 export const App = () => {
   return (
-    <>
+    <Layout>
       <RecipeList items={recipes} />
-    </>
+    </Layout>
   );
 };
