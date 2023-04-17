c12d50db0ec5cb7c63214ed9786a31b5d21c4f67
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-04-17 22:44:40 +0300

    Update RecipeCard.styled.jsx

--- src/components/RecipeCard/RecipeCard.styled.jsx
+++ src/components/RecipeCard/RecipeCard.styled.jsx
@@ -1,4 +1,22 @@
 import styled from '@emotion/styled';
+
+export const Container = styled.section`
+  position: relative;
+  border: 1px solid black;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  border-radius: 4px;
+  overflow: hidden;
+`;
+
+export const Image = styled.img`
+  display: block;
+  width: 100%;
+  height: 120px;
+  object-fit: cover;
+`;
+
 export const Name = styled.h2`
   margin-top: 8px;
   margin-bottom: 12px;
@@ -56,3 +74,11 @@ export const DifficultyBadge = styled.li`
 //   background-color: ${p => (p.active ? p.theme.colors.accent : 'white')};
 //   color: ${p => (p.active ? 'white' : 'black')};
 // `;
+
+export const Actions = styled.div`
+  position: absolute;
+  top: 8px;
+  right: 8px;
+  display: flex;
+  gap: 4px;
+`;
