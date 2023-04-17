68ff7adaeff3badf1f97fe3bf3e85c21b3c2369d
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-04-17 23:03:44 +0300

    Update RecipeList.styled.jsx

--- src/components/RecipeList/RecipeList.styled.jsx
+++ src/components/RecipeList/RecipeList.styled.jsx
@@ -3,5 +3,11 @@ import styled from '@emotion/styled';
 export const List = styled.ul`
   display: flex;
   flex-wrap: wrap;
-  gap: 8px;
+  gap: 16px;
+  margin: 0;
+  padding: 0;
+`;
+
+export const ListItem = styled.li`
+  width: 320px;
 `;
