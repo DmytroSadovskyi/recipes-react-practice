cbbe86101ca8f17238c8f37b149ca4db9fac7dda
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-04-17 22:44:44 +0300

    Update RecipeList.jsx

--- src/components/RecipeList/RecipeList.jsx
+++ src/components/RecipeList/RecipeList.jsx
@@ -1,12 +1,12 @@
 import { RecipeCard } from '../RecipeCard/RecipeCard';
 import PropTypes from 'prop-types';
 import { List } from './RecipeList.styled';
-export const RecipeList = ({ items }) => {
+export const RecipeList = ({ items, onDelete }) => {
   return (
     <List>
       {items.map(item => (
         <li key={item.id}>
-          <RecipeCard item={item} />
+          <RecipeCard item={item} onDelete={onDelete} />
         </li>
       ))}
     </List>
@@ -19,4 +19,5 @@ RecipeList.propTypes = {
       id: PropTypes.number.isRequired,
     })
   ).isRequired,
+  onDelete: PropTypes.func.isRequired,
 };
